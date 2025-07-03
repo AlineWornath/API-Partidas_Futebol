3d86fec6e6497d70a1d22f95bab387046ab08c3d
alinewornath <aline.wornath@gmail.com>
2025-07-03 12:26:17 -0300

    Adiciona integracao com via cep para endereco de estadio

---
+++ src/main/java/com/NeoCamp/soccer_matches/valueobject/Address.java
@@ -0,0 +1,19 @@
+package com.neocamp.soccer_matches.valueobject;
+
+import com.neocamp.soccer_matches.enums.StateCode;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    private String cep;
+    private String street;
+    private String neighborhood;
+    private String city;
+    private StateCode stateCode;
+}
